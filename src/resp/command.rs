9224add3e07cba2540832f6d7d5ae3e89cedd91a
Stanisław Borowy <stabor@startmail.com>
2025-06-22 13:21:23 +0200

    initial Command implementation

--- src/resp/command.rs
+++ src/resp/command.rs
@@ -1,9 +1,63 @@
-pub enum Command {
-    Set {
-        key: String,
-        value: String,
-    },
-    Get {
-        key: String
+#[derive(Debug, PartialEq)]
+pub enum Command<'a> {
+    Set { key: &'a str, value: &'a str },
+    Get { key: &'a str },
+}
+
+impl<'a> Command<'a> {
+    fn from_parts(parts: Vec<&str>) -> Result<Command, CommandError> {
+        let command = parts.get(0).map(|s| s.as_ref()).unwrap_or("");
+
+        match command {
+            "SET" => {
+                if parts.len() < 3 {
+                    return Err(CommandError::MissingArguments);
+                }
+                Ok(Command::Set {
+                    key: parts[1],
+                    value: parts[2],
+                })
+            }
+
+            "GET" => {
+                if parts.len() < 2 {
+                    return Err(CommandError::MissingArguments);
+                }
+                Ok(Command::Get { key: parts[1] })
+            }
+
+            _ => Err(CommandError::InvalidCommand),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum CommandError {
+    InvalidCommand,
+    MissingArguments,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_command_from_parts() {
+        let parts = vec!["SET", "mykey", "myvalue"];
+        let command = Command::from_parts(parts);
+        assert_eq!(
+            Ok(Command::Set {
+                key: "mykey",
+                value: "myvalue"
+            }),
+            command
+        );
+    }
+
+    #[test]
+    fn test_get_command_from_parts() {
+        let parts = vec!["GET", "mykey"];
+        let command = Command::from_parts(parts);
+        assert_eq!(Ok(Command::Get { key: "mykey" }), command);
     }
 }
